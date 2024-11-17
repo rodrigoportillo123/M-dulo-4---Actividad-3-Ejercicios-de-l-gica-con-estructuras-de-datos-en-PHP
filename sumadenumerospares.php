c202bf12d3f5a837ff215aa76adfa927ce24f5ce
carlos portillo <carlitosportillo123@gmail.com>
2024-11-17 11:48:49 -0300

    creacion de suma de numeros pares

---
+++ sumadenumerospares.php
@@ -0,0 +1,20 @@
+<?php
+    
+    $numeros= array("1", "2", "3", "4", "5", "6", "7","8");
+    //corremos los elementos del array
+    foreach ($numeros as $numeros) {
+            //comprobamos que los elementos sean dividos por dos
+        if ($numeros % 2==0) {
+            //obtencion del numero par
+            echo $numeros;
+           
+        }
+              
+              
+               
+            }
+
+       
+
+
+?>
