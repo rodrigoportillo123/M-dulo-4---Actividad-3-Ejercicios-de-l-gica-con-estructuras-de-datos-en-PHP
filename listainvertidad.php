88666f5ccef8b8f52fd2d13bb37d82f050867684
carlos portillo <carlitosportillo123@gmail.com>
2024-11-17 11:21:54 -0300

    Creacion de lista invertida de numeros

---
+++ listainvertidad.php
@@ -0,0 +1,9 @@
+<?php
+
+ //array con lista de numero
+$numero= array("1", "2", "3", "4","5");
+//con el codigo array_reverse pondra en inverso los nummeros
+print_r(array_reverse($numero));
+
+
+?>
